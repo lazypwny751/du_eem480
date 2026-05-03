9ebf15d0c06ac0ce782f54fc261a6ff6491c1472
lazypwny751 <testfordnshijacking@gmail.com>
2026-05-04 00:49:21 +0300

    formatted sources for 11 week

--- 11_debouncing_circuit/db_fsm.vhdl
+++ 11_debouncing_circuit/db_fsm.vhdl
@@ -1,121 +1,206 @@
+-- ============================================================================
+-- Debouncing Devresi - VHDL Uygulaması
+-- ============================================================================
+-- Amaç: Tuş/Switch giriş sinyalinin mekanik titreşim etkisini gidermek
+-- Zaman Dilimi: 10ms (2^19 * 20ns) x3 = 30ms debounce süresi
+-- ============================================================================
+
+-- ============================================================================
 -- Kütüphaneler
+-- ============================================================================
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
--- Entity: Debouncing Devresi
+-- ============================================================================
+-- Entity: Debouncing Devresi (db_fsm)
+-- ============================================================================
 -- Giriş sinyalinin 30ms boyunca istikrarlı kaldığında çıkışı güncelleme
 entity db_fsm is
- port (
-  clk, reset: in std_logic;      -- Saat ve reset sinyalleri
-  sw: in std_logic;              -- Tuş/Switch giriş sinyali
-  db: out std_logic              -- Debounce çıkış sinyali
- );
+  port (
+    clk       : in  std_logic;   -- Sistem saati (50 MHz)
+    reset     : in  std_logic;   -- Asenkron reset (aktif yüksek)
+    sw        : in  std_logic;   -- Tuş/Switch giriş sinyali
+    db        : out std_logic    -- Debounce edilmiş çıkış sinyali
+  );
 end db_fsm;
 
+-- ============================================================================
+-- Mimari: FSM Tabanlı Debouncing Devresi
+-- ============================================================================
 architecture arch of db_fsm is
- constant N: integer:=19; -- 2^N * 20ns = 10ms
- signal q_reg, q_next: unsigned(N-1 downto 0);
- signal m_tick: std_logic;
- type eg_state_type is (zero, wait1_1, wait1_2, wait1_3, one, wait0_1, wait0_2, wait0_3);
- signal state_reg, state_next: eg_state_type;
-begin
- -- Sayıcı: 10ms zaman dilimi oluşturma (2^19 * 20ns)
- process(clk,reset)
- begin
- if (clk'event and clk='1') then
- q_reg <= q_next;
- end if;
- end process;
- -- Sayıcının sonraki durumu
- q_next <= q_reg + 1;
- -- 10ms tick sinyali çıkışı
- m_tick <= '1' when q_reg=0 else '0';
 
- -- Debouncing Sonlu Durum Makinesi (FSM)
- -- Durum kaydedici
- process(clk,reset)
- begin
- if (reset='1') then
- state_reg <= zero;
- elsif (clk'event and clk='1') then
- state_reg <= state_next;
- end if;
- end process;
- -- Sonraki durum ve çıkış mantığı
- process(state_reg,sw,m_tick)
- begin
- state_next <= state_reg; -- Varsayılan: aynı duruma geri dön
- db <= '0'; -- Varsayılan çıkış 0
- case state_reg is
- -- Durumu: Tuş basılmamış (0)
- when zero =>
- if sw='1' then
- state_next <= wait1_1;
- end if;
- -- Bekle 1. aşama: Tuş basılı kalıyor mu? (1. tick)
- when wait1_1 =>
- if sw='0' then
- state_next <= zero;
- else
- if m_tick='1' then
- state_next <= wait1_2;
- end if;
- end if;
- -- Bekle 1. aşama: Tuş basılı kalıyor mu? (2. tick)
- when wait1_2 =>
- if sw='0' then
- state_next <= zero;
- else
- if m_tick='1' then
- state_next <= wait1_3;
- end if;
- end if;
- -- Bekle 1. aşama: Tuş basılı kalıyor mu? (3. tick - tıklamada kararda)
- when wait1_3 =>
- if sw='0' then
- state_next <= zero;
- else
- if m_tick='1' then
- state_next <= one;
- end if;
- end if;
- -- Durumu: Tuş basıldı (1)
- when one =>
- db <= '1';
- if sw='0' then
- state_next <= wait0_1;
- end if;
- -- Bekle 0. aşama: Tuş bırakıldı mı? (1. tick)
- when wait0_1 =>
- db <= '1';
- if sw='1' then
- state_next <= one;
- else
- if m_tick='1' then
- state_next <= wait0_2;
- end if;
- end if;
- -- Bekle 0. aşama: Tuş bırakıldı mı? (2. tick)
- when wait0_2 =>
- db <= '1';
- if sw='1' then
- state_next <= one;
- else
- if m_tick='1' then
- state_next <= wait0_3;
- end if;
- end if;
- -- Bekle 0. aşama: Tuş bırakıldı mı? (3. tick - bırakışta kararda)
- when wait0_3 =>
- db <= '1';
- if sw='1' then
- state_next <= one;
- else
- if m_tick='1' then
- state_next <= zero;
- end if;
- end if;
- end case;
- end process;
+  -- ========================================================================
+  -- Sabitler ve Sinyaller
+  -- ========================================================================
+  constant N : integer := 19;  -- 2^N * 20ns = 10ms zaman dilimi
+
+  -- Sayıcı sinyalleri (10ms tick oluşturmak için)
+  signal q_reg, q_next : unsigned(N-1 downto 0);
+  signal m_tick        : std_logic;
+
+  -- FSM durum tanımı (8 durum: 0 veya 1 ile 30ms geçiş süresi)
+  type eg_state_type is (
+    zero,      -- Tuş basılmamış (stabil durumu)
+    wait1_1,   -- Bekle durum 1/3: 10ms
+    wait1_2,   -- Bekle durum 2/3: 20ms
+    wait1_3,   -- Bekle durum 3/3: 30ms (sonra "one"'a geç)
+    one,       -- Tuş basıldı (stabil durumu)
+    wait0_1,   -- Bekle durum 1/3: 10ms
+    wait0_2,   -- Bekle durum 2/3: 20ms
+    wait0_3    -- Bekle durum 3/3: 30ms (sonra "zero"'a geç)
+  );
+
+  signal state_reg, state_next : eg_state_type;
+
+begin
+
+  -- ==========================================================================
+  -- BLOK 1: 10ms Zaman Dilimi Sayıcısı
+  -- ==========================================================================
+  -- Amaç: Her 10ms'de bir tick sinyali üret
+  -- Sayıcı, 2^19 tick sayıldığında reset olur
+  -- ==========================================================================
+
+  -- Sayıcı kaydedici (clk yükselen kenarında güncelle)
+  process (clk, reset)
+  begin
+    if (reset = '1') then
+      q_reg <= (others => '0');
+    elsif (clk'event and clk = '1') then
+      q_reg <= q_next;
+    end if;
+  end process;
+
+  -- Sayıcı mantığı: sayı sıfırdan başlayarak artır, max'a ulaşınca sıfırla
+  q_next <= (others => '0') when (q_reg = (2**N - 1)) else (q_reg + 1);
+
+  -- Tick sinyali: Sayıcı sıfırda olduğunda high
+  m_tick <= '1' when (q_reg = 0) else '0';
+
+  -- ==========================================================================
+  -- BLOK 2: Durum Makinesi (FSM) - Debouncing Mantığı
+  -- ==========================================================================
+  -- Amaç: Tuş sinyalini 30ms boyunca stabil olması koşuluyla izlemek
+  -- Stabil durumda: 0 (basılmamış) veya 1 (basıldı)
+  -- Geçiş durumları: wait1_1, wait1_2, wait1_3 (0->1) ve wait0_1, wait0_2, wait0_3 (1->0)
+  -- ==========================================================================
+
+  -- Durum kaydedici (clk yükselen kenarında güncelle)
+  process (clk, reset)
+  begin
+    if (reset = '1') then
+      state_reg <= zero;  -- Başlangıçta tuş basılmamış
+    elsif (clk'event and clk = '1') then
+      state_reg <= state_next;
+    end if;
+  end process;
+
+  -- Durum geçiş ve çıkış mantığı
+  process (state_reg, sw, m_tick)
+  begin
+    -- Varsayılan değerler
+    state_next <= state_reg;  -- Durumda kal
+    db <= '0';                -- Çıkış 0
+
+    case state_reg is
+
+      -- ====================================================================
+      -- DURUM: ZERO (Tuş Basılmamış - Stabil)
+      -- ====================================================================
+      when zero =>
+        db <= '0';
+        if (sw = '1') then
+          -- Tuş basıldı! Geçiş durumuna git
+          state_next <= wait1_1;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: WAIT1_1 (Tuş Basılı Geçiş - 1/3: 10ms)
+      -- ====================================================================
+      when wait1_1 =>
+        if (sw = '0') then
+          -- Tuş bırakıldı, geri dön (titreşim yoksayıldı)
+          state_next <= zero;
+        elsif (m_tick = '1') then
+          -- 10ms geçti, ikinci aşamaya git
+          state_next <= wait1_2;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: WAIT1_2 (Tuş Basılı Geçiş - 2/3: 20ms)
+      -- ====================================================================
+      when wait1_2 =>
+        if (sw = '0') then
+          -- Tuş bırakıldı, geri dön (titreşim yoksayıldı)
+          state_next <= zero;
+        elsif (m_tick = '1') then
+          -- 20ms geçti, üçüncü aşamaya git
+          state_next <= wait1_3;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: WAIT1_3 (Tuş Basılı Geçiş - 3/3: 30ms - Final Kontrol)
+      -- ====================================================================
+      when wait1_3 =>
+        if (sw = '0') then
+          -- Tuş bırakıldı, geri dön (titreşim yoksayıldı)
+          state_next <= zero;
+        elsif (m_tick = '1') then
+          -- 30ms boyunca stabil kaldı! Debounce tamam
+          state_next <= one;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: ONE (Tuş Basıldı - Stabil)
+      -- ====================================================================
+      when one =>
+        db <= '1';  -- Çıkış high
+        if (sw = '0') then
+          -- Tuş bırakıldı! Geçiş durumuna git
+          state_next <= wait0_1;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: WAIT0_1 (Tuş Bırakılı Geçiş - 1/3: 10ms)
+      -- ====================================================================
+      when wait0_1 =>
+        db <= '1';  -- Henüz çıkış high (debounce için)
+        if (sw = '1') then
+          -- Tuş basıldı, geri dön (titreşim yoksayıldı)
+          state_next <= one;
+        elsif (m_tick = '1') then
+          -- 10ms geçti, ikinci aşamaya git
+          state_next <= wait0_2;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: WAIT0_2 (Tuş Bırakılı Geçiş - 2/3: 20ms)
+      -- ====================================================================
+      when wait0_2 =>
+        db <= '1';  -- Henüz çıkış high (debounce için)
+        if (sw = '1') then
+          -- Tuş basıldı, geri dön (titreşim yoksayıldı)
+          state_next <= one;
+        elsif (m_tick = '1') then
+          -- 20ms geçti, üçüncü aşamaya git
+          state_next <= wait0_3;
+        end if;
+
+      -- ====================================================================
+      -- DURUM: WAIT0_3 (Tuş Bırakılı Geçiş - 3/3: 30ms - Final Kontrol)
+      -- ====================================================================
+      when wait0_3 =>
+        db <= '1';  -- Henüz çıkış high (debounce için)
+        if (sw = '1') then
+          -- Tuş basıldı, geri dön (titreşim yoksayıldı)
+          state_next <= one;
+        elsif (m_tick = '1') then
+          -- 30ms boyunca stabil kaldı! Debounce tamam
+          state_next <= zero;
+        end if;
+
+    end case;
+  end process;
+
 end arch;
--- 11_debouncing_circuit/db_fsm_tb.vhdl
+++ 11_debouncing_circuit/db_fsm_tb.vhdl
@@ -1,91 +1,199 @@
--- Test Bench: Debouncing Devresi (db_fsm) Test Dosyası
--- Bu dosya debouncing devresi simülasyonunu sağlar
+-- ============================================================================
+-- Test Bench: Debouncing Devresi (db_fsm) - Simülasyon Test Dosyası
+-- ============================================================================
+-- Amaç: db_fsm bileşenini test etmek ve debouncing işlemini doğrulamak
+-- Testler: Basılı/bırakılı, titreşim yoksayma, hızlı geçişler
+-- ============================================================================
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+-- ============================================================================
+-- Entity: db_fsm_tb (Test Bench)
+-- ============================================================================
 entity db_fsm_tb is
 end db_fsm_tb;
 
+-- ============================================================================
+-- Mimari: Simülasyon Testi
+-- ============================================================================
 architecture tb of db_fsm_tb is
- -- Bileşen bildirimi
- component db_fsm is
-  port (
-   clk, reset: in std_logic;
-   sw: in std_logic;
-   db: out std_logic
-  );
- end component;
- 
- -- Test sinyalleri
- signal clk_tb, reset_tb: std_logic;
- signal sw_tb: std_logic;
- signal db_tb: std_logic;
- constant CLK_PERIOD: time := 20 ns; -- 50 MHz saat
- 
+
+  -- ========================================================================
+  -- Bileşen Bildirimi (Component Declaration)
+  -- ========================================================================
+  component db_fsm is
+    port (
+      clk   : in  std_logic;
+      reset : in  std_logic;
+      sw    : in  std_logic;
+      db    : out std_logic
+    );
+  end component;
+
+  -- ========================================================================
+  -- Test Sinyalleri (Signals)
+  -- ========================================================================
+  signal clk_tb      : std_logic;
+  signal reset_tb    : std_logic;
+  signal sw_tb       : std_logic;
+  signal db_tb       : std_logic;
+
+  -- Saat periyodu: 20ns (50 MHz)
+  constant CLK_PERIOD : time := 20 ns;
+
 begin
- -- db_fsm bileşenini örneği
- uut: db_fsm port map (
-  clk => clk_tb,
-  reset => reset_tb,
-  sw => sw_tb,
-  db => db_tb
- );
- 
- -- Saat üreteç
- process
- begin
-  clk_tb <= '0';
-  wait for CLK_PERIOD/2;
-  clk_tb <= '1';
-  wait for CLK_PERIOD/2;
- end process;
- 
- -- Test uyarıları
- process
- begin
-  -- Başlangıç durumu
-  reset_tb <= '1';
-  sw_tb <= '0';
-  wait for 100 ns;
-  
-  reset_tb <= '0';
-  wait for 100 ns;
-  
-  -- Test 1: Tuş basılı, 40ms boyunca (debounc edilecek)
-  report "Test 1: Tus basili sinyali - debounce testi";
-  sw_tb <= '1';
-  wait for 40 ms;
-  
-  -- Test 2: Tuş bırakılı
-  report "Test 2: Tus birakili sinyali - debounce testi";
-  sw_tb <= '0';
-  wait for 40 ms;
-  
-  -- Test 3: Kısa titreşim testi (debounce tarafından yoksayılacak)
-  report "Test 3: Kisa titresim testi";
-  sw_tb <= '1';
-  wait for 5 ms;
-  sw_tb <= '0';
-  wait for 5 ms;
-  sw_tb <= '1';
-  wait for 5 ms;
-  sw_tb <= '0';
-  wait for 50 ms;
-  
-  -- Test 4: Tekrarlayan basılı/bırakılı
-  report "Test 4: Tekrarlayan tus testi";
-  for i in 1 to 3 loop
-   sw_tb <= '1';
-   wait for 50 ms;
-   sw_tb <= '0';
-   wait for 50 ms;
-  end loop;
-  
-  -- Simülasyonu bitir
-  report "Simülasyon tamamlandı";
-  wait;
- end process;
- 
+
+  -- ========================================================================
+  -- UUT (Unit Under Test): db_fsm Bileşenini Örneği Oluştur
+  -- ========================================================================
+  uut : db_fsm
+    port map (
+      clk   => clk_tb,
+      reset => reset_tb,
+      sw    => sw_tb,
+      db    => db_tb
+    );
+
+  -- ========================================================================
+  -- BLOK 1: Saat Üreticisi (Clock Generator)
+  -- ========================================================================
+  -- Açıklama: 50 MHz sistemik saat sinyali üret (20ns periyot)
+  -- ========================================================================
+  process
+  begin
+    clk_tb <= '0';
+    wait for CLK_PERIOD / 2;
+    clk_tb <= '1';
+    wait for CLK_PERIOD / 2;
+  end process;
+
+  -- ========================================================================
+  -- BLOK 2: Test Uyarıları (Test Stimulus)
+  -- ========================================================================
+  -- Açıklama: Çeşitli tuş basılı/bırakılı senaryolarını test et
+  -- ========================================================================
+  process
+  begin
+    -- ====================================================================
+    -- BAŞLANGIÇ DURUMU
+    -- ====================================================================
+    report "========================================";
+    report "Debouncing Devresi Test Başlıyor";
+    report "========================================";
+
+    -- Reset sinyali aktif et (100ns)
+    reset_tb <= '1';
+    sw_tb <= '0';
+    wait for 100 ns;
+
+    -- Reset sinyali pasif et
+    reset_tb <= '0';
+    wait for 100 ns;
+
+    report "Başlangıç: Reset tamamlandı, sistem hazır";
+
+    -- ====================================================================
+    -- TEST 1: Basit Tuş Basılı/Bırakılı
+    -- ====================================================================
+    report "";
+    report "TEST 1: Basit Tuş Basılı Sinyali (40ms)";
+    report "Beklenen: Çıkış db 30ms gecikmeli olarak aktif olmalı";
+    report "-------------------------------------------";
+
+    sw_tb <= '1';  -- Tuşu bas
+    wait for 40 ms;
+
+    assert db_tb = '1'
+      report "HATA: db çıkışı 40ms sonra '1' olmalı!" severity error;
+
+    report "TEST 1 BAŞARILI: db çıkışı aktif";
+
+    -- ====================================================================
+    -- TEST 2: Tuş Bırakılması
+    -- ====================================================================
+    report "";
+    report "TEST 2: Tuş Bırakılması (40ms)";
+    report "Beklenen: Çıkış db 30ms gecikmeli olarak pasif olmalı";
+    report "-------------------------------------------";
+
+    sw_tb <= '0';  -- Tuşu bırak
+    wait for 40 ms;
+
+    assert db_tb = '0'
+      report "HATA: db çıkışı 40ms sonra '0' olmalı!" severity error;
+
+    report "TEST 2 BAŞARILI: db çıkışı pasif";
+
+    -- ====================================================================
+    -- TEST 3: Kısa Titreşim Testi (Debounce Yoksayma)
+    -- ====================================================================
+    report "";
+    report "TEST 3: Kısa Titreşim Testi (5ms pulse'lar)";
+    report "Beklenen: Kısa pulse'lar debounce tarafından yoksayılmalı";
+    report "-------------------------------------------";
+
+    -- Üç tane hızlı pulse gönder (her biri 5ms)
+    sw_tb <= '1';
+    wait for 5 ms;
+
+    sw_tb <= '0';
+    wait for 5 ms;
+
+    sw_tb <= '1';
+    wait for 5 ms;
+
+    sw_tb <= '0';
+    wait for 5 ms;
+
+    -- Bu noktada titreşim henüz yoksayılmış olmalı
+    assert db_tb = '0'
+      report "HATA: Kısa pulse'lar debounce tarafından yoksayılmalı!" severity error;
+
+    report "TEST 3 BAŞARILI: Titreşim yoksayıldı";
+
+    -- ====================================================================
+    -- TEST 4: Tekrarlayan Tuş Basışı (3 kere)
+    -- ====================================================================
+    report "";
+    report "TEST 4: Tekrarlayan Tuş Basışı (50ms aralıklar)";
+    report "Beklenen: Her basılı/bırakılı çiftinde 30ms gecikmeli yanıt";
+    report "-------------------------------------------";
+
+    for i in 1 to 3 loop
+      report "  Döngü " & integer'image(i) & ": Tuş basıl";
+      sw_tb <= '1';
+      wait for 50 ms;
+
+      assert db_tb = '1'
+        report "HATA: db çıkışı döngü " & integer'image(i) & " basılı sırasında '1' olmalı!" 
+        severity error;
+
+      report "  Döngü " & integer'image(i) & ": Tuş bırak";
+      sw_tb <= '0';
+      wait for 50 ms;
+
+      assert db_tb = '0'
+        report "HATA: db çıkışı döngü " & integer'image(i) & " bırakılı sırasında '0' olmalı!" 
+        severity error;
+
+      report "  Döngü " & integer'image(i) & " BAŞARILI";
+    end loop;
+
+    report "TEST 4 BAŞARILI: Tekrarlayan tuş testleri geçildi";
+
+    -- ====================================================================
+    -- SONUÇ
+    -- ====================================================================
+    report "";
+    report "========================================";
+    report "TÜM TESTLER BAŞARILI!";
+    report "Debouncing Devresi Doğru Çalışıyor";
+    report "========================================";
+
+    wait;
+
+  end process;
+
 end tb;
